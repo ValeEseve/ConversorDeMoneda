fc0af3e4b7052616e8d3207feb1c859cfb3d1f74
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-05-09 00:47:36 -0400

    Btn conversion listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,36 @@
-// Obtener el monto a convertir
-// Seleccionar la moneda
-// Presionar botón de convertir
-    // Obtener valor de la moneda
+// Variables
+const btn = document.querySelector("#btn-convertir")
+
+// Funciones
+async function convertirMoneda() {
+    const cifra = Number(document.querySelector("#input-cifra").value)
+    const moneda = document.querySelector("#select-moneda").value.toString()
+    console.log(moneda)
+    let resultadoTxt = document.querySelector("#resultado-conversion")
+    const valorMoneda = await obtenerValorMoneda(moneda)
+    console.log(valorMoneda)
+    resultado = await cifra/valorMoneda
+    console.log(resultado)
+    resultadoTxt.innerHTML = `$ ${resultado}`    
+}
+
 const obtenerValorMoneda = async (moneda) => {
     url = "https://mindicador.cl/api/"+moneda.toString()
+    console.log(url)
     try {
         const res = await fetch(url)
-        const valor = await res.json()
-        return valor
+        const data = await res.json()
+        const valorMoneda = data.serie[0].valor
+        return valorMoneda
     } catch (error) {
         alert("No se puede obtener el valor de la moneda seleccionada en este momento.")
     }
 }
-// Realizar cálculo de conversión
-// Obtener la conversión
-// Mostrar el monto convertido
+
 // Obtener el valor de la moneda seleccionada de los últimos diez días
 // Configurar grafico Chart.js
 // Mostrar gráfico
 
+// Eventos
+
+btn.addEventListener("click", convertirMoneda)
